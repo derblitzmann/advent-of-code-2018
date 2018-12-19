38d7f87171f708d423b73f8d5cb57534dc003608
Thor <t.steingrimsson@f5.com>
2018-12-18 17:10:17 -0800

    Finished part 2 of day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 def main(filename):
     freq = 0
@@ -8,10 +8,25 @@ def main(filename):
             freq += change
     print('Resulting frequency: {}'.format(freq))
 
+def main2(filename):
+    freq = 0
+    freq_history = {0: True}
+    while True:
+        with open(filename) as f:
+            for line in f.readlines():
+                change = int(line)
+                freq += change
+                if freq not in freq_history:
+                    freq_history[freq] = True
+                else:
+                    print('Found first repeated frequency: {}'.format(freq))
+                    return
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 2:
         print('Need filename to run')
         sys.exit(1)
-    main(sys.argv[1])
+    filename = sys.argv[1]
+    main(filename)
+    main2(filename)
